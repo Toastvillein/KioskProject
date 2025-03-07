3e3fa97b90c280399d04822c89823face33ae6e0
Toastvillein <okkcba7899@gmail.com>
2025-03-07 20:00:13 +0900

    KioskProject lv2.1

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -1,16 +1,30 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<MenuItem> itemList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        MenuItem menuItem1 = new MenuItem("ShackBurger",6900,"토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        MenuItem menuItem2 = new MenuItem("SmokeShack",8900,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        MenuItem menuItem3 = new MenuItem("Cheeseburger",6900,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+        MenuItem menuItem4 = new MenuItem("Hamburger",5400,"비프패티를 기반으로 야채가 들어간 기본버거");
+        itemList.add(menuItem1);
+        itemList.add(menuItem2);
+        itemList.add(menuItem3);
+        itemList.add(menuItem4);
+
+
         while (true) {
             System.out.println("[ SHAKESHAK MENU ]");
-            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-            System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            System.out.println("1."+ itemList.get(0));
+            System.out.println("2."+ itemList.get(1));
+            System.out.println("3."+ itemList.get(2));
+            System.out.println("4."+ itemList.get(3));
             System.out.println("0. 종료      | 종료");
             int choice = scanner.nextInt();
 
--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -1,4 +1,25 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuItem {
+    // 버거 이름 가격 설명 선언
+    private String burgerName;
+    private int burgerPrice;
+    private String burgerDescription;
+
+    public MenuItem (String burgerName,int burgerPrice,String burgerDescription) {
+        this.burgerName = burgerName;
+        this.burgerPrice = burgerPrice;
+        this.burgerDescription = burgerDescription;
+    }
+
+    public String toString() {
+        return burgerName+" "+burgerPrice+"원"+"\n"+burgerDescription;
+    }
+
+    // 버거 이름 버거 가격 버거 설명 각각 따로 호출하는 메서드 작성
+
+
 }
