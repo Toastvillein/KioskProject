7e44e18bedbf467ba220d08309f3048c0d51cc72
Toastvillein <okkcba7899@gmail.com>
2025-03-10 15:46:03 +0900

    KioskProject lv3.0

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -1,4 +1,42 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Kiosk {
+    List<MenuItem> itemList = new ArrayList<>();
+
+    // List의 생성자
+    public Kiosk(List<MenuItem> itemList) {
+        this.itemList = itemList;
+    }
+
+    // start() 메서드
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("[ SHAKESHAK MENU ]");
+
+            for (int i=0; i< itemList.size(); i++ ) {
+                System.out.print(i+1+". ");
+                System.out.println(itemList.get(i));
+            }
+            System.out.println("0. 종료      | 종료");
+            int choice = scanner.nextInt();
+
+            if(choice!=0){
+                try {
+                    System.out.println(itemList.get(choice-1).getBurgerName() + "를 고르셨습니다. " + itemList.get(choice-1).getBurgerPrice() + "원입니다.");
+                    Thread.sleep(1500);
+                } catch (Exception e) {
+                    System.out.println("해당하는 메뉴가 없습니다.");
+                    throw new RuntimeException(e);
+                }
+            } else if (choice==0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+        }
+    }
 }
--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -1,52 +1,20 @@
 package com.example.kiosk;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<MenuItem> itemList = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        MenuItem menuItem1 = new MenuItem("ShackBurger",6900,"토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        MenuItem menuItem2 = new MenuItem("SmokeShack",8900,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        MenuItem menuItem3 = new MenuItem("Cheeseburger",6900,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        MenuItem menuItem4 = new MenuItem("Hamburger",5400,"비프패티를 기반으로 야채가 들어간 기본버거");
-        itemList.add(menuItem1);
-        itemList.add(menuItem2);
-        itemList.add(menuItem3);
-        itemList.add(menuItem4);
-
-
-        while (true) {
-            System.out.println("[ SHAKESHAK MENU ]");
-            System.out.println("1."+ itemList.get(0));
-            System.out.println("2."+ itemList.get(1));
-            System.out.println("3."+ itemList.get(2));
-            System.out.println("4."+ itemList.get(3));
-            System.out.println("0. 종료      | 종료");
-            int choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.println(menuItem1.getBurgerName()+"를 고르셨습니다. "+menuItem1.getBurgerPrice()+"원입니다.");
-                    continue;
-                case 2:
-                    System.out.println(menuItem2.getBurgerName()+"를 고르셨습니다. "+menuItem2.getBurgerPrice()+"원입니다.");
-                    continue;
-                case 3:
-                    System.out.println(menuItem3.getBurgerName()+"를 고르셨습니다. "+menuItem3.getBurgerPrice()+"원입니다.");
-                    continue;
-                case 4:
-                    System.out.println(menuItem4.getBurgerName()+"를 고르셨습니다. "+menuItem4.getBurgerPrice()+"원입니다.");
-                    continue;
-                case 0:
-                    System.out.println("프로그램을 종료합니다.");
-                    continue;
-                default:
-                    System.out.println("잘못입력하셨습니다");
-            }
-        }
+        List<MenuItem> menuItems = new ArrayList<>(
+                List.of(
+                        new MenuItem("ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"),
+                        new MenuItem("SmokeBurger", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"),
+                        new MenuItem("CheeseBurger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"),
+                        new MenuItem("Hamburger", 5400, "비프패티를 기반으로 야채가 들어간 기본버거")
+                )
+        );
+        Kiosk kiosk = new Kiosk(menuItems);
+        kiosk.start();
     }
 }
+
